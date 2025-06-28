d7d673185c8a2687297ceb2cb7784f387f564898
Gyula <erdeigyula3@gmail.com>
2025-06-28 13:03:06 +0200

    Update EnchantmentUtils.java

--- src/main/java/net/farkas/wildaside/util/EnchantmentUtils.java
+++ src/main/java/net/farkas/wildaside/util/EnchantmentUtils.java
@@ -6,16 +6,24 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 
 public class EnchantmentUtils {
     public static Holder<Enchantment> getEnchtantmentHolder(Level level, ResourceKey<Enchantment> resourceKey) {
-        System.out.println("KAKAKAKAKKA");
+        return Holder.direct(getEnchantment(level, resourceKey));
+    }
+
+    public static Holder<Enchantment> getEnchtantmentHolder(LevelAccessor level, ResourceKey<Enchantment> resourceKey) {
         return Holder.direct(getEnchantment(level, resourceKey));
     }
 
     public static Enchantment getEnchantment(Level level, ResourceKey<Enchantment> resourceKey) {
         HolderLookup.RegistryLookup<Enchantment> registrylookup = level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
-        System.out.println(registrylookup.getOrThrow(resourceKey));
+        return registrylookup.getOrThrow(resourceKey).get();
+    }
+
+    public static Enchantment getEnchantment(LevelAccessor level, ResourceKey<Enchantment> resourceKey) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
         return registrylookup.getOrThrow(resourceKey).get();
     }
 }
