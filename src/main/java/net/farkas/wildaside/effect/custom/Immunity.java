0a8dd5cee98b22370bc47c17d2f99e01c5a64016
Gyula <erdeigyula3@gmail.com>
2025-06-28 13:08:36 +0200

    start of 2.1.1

--- src/main/java/net/farkas/wildaside/effect/custom/Immunity.java
+++ src/main/java/net/farkas/wildaside/effect/custom/Immunity.java
@@ -1,32 +1,11 @@
 package net.farkas.wildaside.effect.custom;
 
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.LivingEntity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Immunity extends MobEffect {
     public Immunity(MobEffectCategory pCategory, int pColor) {
         super(pCategory, pColor);
     }
 
-    @Override
-    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
-        if (!pLivingEntity.level().isClientSide()) {
-            if (pLivingEntity instanceof ServerPlayer serverPlayer) {
-                List<MobEffectInstance> effects = new ArrayList<>(serverPlayer.getActiveEffects());
-                for (MobEffectInstance effectInstance : effects) {
-                    if (!effectInstance.getEffect().isBeneficial() && effectInstance.getEffect() != this) {
-                        serverPlayer.removeEffect(effectInstance.getEffect());
-                    }
-                }
-            }
-        }
-
-        super.applyEffectTick(pLivingEntity, pAmplifier);
-    }
 }
