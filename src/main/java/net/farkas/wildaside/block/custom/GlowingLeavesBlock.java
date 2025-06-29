0b477e7f08304433ec91c4378480a3be28166782
Gyula <erdeigyula3@gmail.com>
2025-06-29 23:03:36 +0200

    fixed fallen leaves spawning underground

--- src/main/java/net/farkas/wildaside/block/custom/GlowingLeavesBlock.java
+++ src/main/java/net/farkas/wildaside/block/custom/GlowingLeavesBlock.java
@@ -147,6 +147,7 @@ public class GlowingLeavesBlock extends LeavesBlock {
                     groundY = y;
                     break;
                 }
+                break;
             }
         }
 
