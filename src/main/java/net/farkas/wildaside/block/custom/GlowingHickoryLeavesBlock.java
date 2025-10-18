0c1ece3f6fe8a945db66e8cddc3e66b0b65dbe33
Gyula <erdeigyula3@gmail.com>
2025-10-18 08:51:13 +0200

    glowing leaves tick config

--- src/main/java/net/farkas/wildaside/block/custom/GlowingHickoryLeavesBlock.java
+++ src/main/java/net/farkas/wildaside/block/custom/GlowingHickoryLeavesBlock.java
@@ -1,6 +1,7 @@
 package net.farkas.wildaside.block.custom;
 
 import net.farkas.wildaside.block.ModBlocks;
+import net.farkas.wildaside.config.Config;
 import net.farkas.wildaside.item.ModItems;
 import net.farkas.wildaside.particle.ModParticles;
 import net.farkas.wildaside.util.GlowingHickoryLightUtil;
@@ -80,7 +81,7 @@ public class GlowingHickoryLeavesBlock extends LeavesBlock {
     public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
         super.tick(pState, pLevel, pPos, pRandom);
 
-        if (pLevel.isClientSide() || pState.getValue(GlowingHickoryLeavesBlock.FIXED_LIGHTING)) return;
+        if (pLevel.isClientSide() || pState.getValue(GlowingHickoryLeavesBlock.FIXED_LIGHTING) || !Config.GLOWING_HICKORY_LEAVES_TICK.get()) return;
 
         int time = (int)pLevel.dayTime();
         int currentLight = pState.getValue(LIGHT);
@@ -91,7 +92,6 @@ public class GlowingHickoryLeavesBlock extends LeavesBlock {
         }
 
         pLevel.scheduleTick(pPos, this, 100);
-
     }
 
     @Override
