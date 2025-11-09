d9cb9fff0cc1d0d2355044dab965ee5b35657520
Gyula <erdeigyula3@gmail.com>
2025-11-09 14:29:20 +0100

    shift click fix

--- src/main/java/net/farkas/wildaside/screen/bioengineering_workstation/BioengineeringWorkstationMenu.java
+++ src/main/java/net/farkas/wildaside/screen/bioengineering_workstation/BioengineeringWorkstationMenu.java
@@ -24,6 +24,9 @@ public class BioengineeringWorkstationMenu extends AbstractContainerMenu {
 
     private BioengineeringWorkstationTab tab;
 
+    private int teFirstSlotIndex;
+    private int teSlotCount;
+
     public BioengineeringWorkstationMenu(int pContainerId, Inventory inv, FriendlyByteBuf extraData) {
         this(pContainerId, inv, inv.player.level().getBlockEntity(extraData.readBlockPos()), new SimpleContainerData(6));
     }
@@ -47,6 +50,8 @@ public class BioengineeringWorkstationMenu extends AbstractContainerMenu {
 
     private void addMenuSlots(BioengineeringWorkstationTab tab) {
         this.blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(iItemHandler -> {
+            int before = slots.size();
+
             switch (tab) {
                 case ASSEMBLER -> {
                     addSlot(new SlotItemHandler(iItemHandler, 0, 84, 34));
@@ -67,6 +72,9 @@ public class BioengineeringWorkstationMenu extends AbstractContainerMenu {
                     addSlot(new BioengineeringWorkstationResultSlot(iItemHandler, 10, 224, 57, player));
                 }
             }
+
+            this.teFirstSlotIndex = before;
+            this.teSlotCount = slots.size() - before;
         });
     }
 
@@ -113,6 +121,7 @@ public class BioengineeringWorkstationMenu extends AbstractContainerMenu {
     }
 
     // CREDIT GOES TO: diesieben07 | https://github.com/diesieben07/SevenCommons
+    // MODIFIED VERSION BY FARKAS!
     // must assign a slot number to each of the slots used by the GUI.
     // For this container, we can see both the tile inventory's slots as well as the player inventory slots and the hotbar.
     // Each time we add a Slot to the container, it automatically increases the slotIndex, which means
@@ -126,43 +135,30 @@ public class BioengineeringWorkstationMenu extends AbstractContainerMenu {
     private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
     private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
     private static final int VANILLA_FIRST_SLOT_INDEX = 0;
-    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
 
-    // THIS YOU HAVE TO DEFINE!
-    private static final int TE_INVENTORY_SLOT_COUNT = 6;  // must be the number of slots you have!
     @Override
     public ItemStack quickMoveStack(Player playerIn, int pIndex) {
         Slot sourceSlot = slots.get(pIndex);
-        if (sourceSlot == null || !sourceSlot.hasItem()) return ItemStack.EMPTY;  //EMPTY_ITEM
-        ItemStack sourceStack = sourceSlot.getItem();
-        ItemStack copyOfSourceStack = sourceStack.copy();
+        if (sourceSlot == null || !sourceSlot.hasItem()) return ItemStack.EMPTY;
 
-        // Check if the slot clicked is one of the vanilla container slots
+        ItemStack sourceStack = sourceSlot.getItem();
+        ItemStack copy = sourceStack.copy();
+
+        int start = teFirstSlotIndex;
+        int end = teFirstSlotIndex + teSlotCount;
+
         if (pIndex < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
-            // This is a vanilla container slot so merge the stack into the tile inventory
-            if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT, false)) {
-                return ItemStack.EMPTY;  // EMPTY_ITEM
-            }
-        }
-        else if (pIndex < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
-            // This is a TE slot so merge the stack into the players inventory
-            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false)) {
+            if (!moveItemStackTo(sourceStack, start, end, false)) return ItemStack.EMPTY;
+        } else if (pIndex < end) {
+            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false))
                 return ItemStack.EMPTY;
-            }
         }
-        else {
-            System.out.println("Invalid slotIndex:" + pIndex);
-            return ItemStack.EMPTY;
-        }
-        // If stack size == 0 (the entire stack was moved) set slot contents to null
-        if (sourceStack.getCount() == 0) {
-            sourceSlot.set(ItemStack.EMPTY);
-        }
-        else {
-            sourceSlot.setChanged();
-        }
+
+        if (sourceStack.isEmpty()) sourceSlot.set(ItemStack.EMPTY);
+        else sourceSlot.setChanged();
+
         sourceSlot.onTake(playerIn, sourceStack);
-        return copyOfSourceStack;
+        return copy;
     }
 
     @Override
