9ea53ca3d80aea03a05122a6d94c860392cb9d2c
Gyula <erdeigyula3@gmail.com>
2025-11-28 22:54:44 +0100

    Update BioengineeringWorkstationBlockEntity.java

--- src/main/java/net/farkas/wildaside/block/entity/BioengineeringWorkstationBlockEntity.java
+++ src/main/java/net/farkas/wildaside/block/entity/BioengineeringWorkstationBlockEntity.java
@@ -224,6 +224,9 @@ public class  BioengineeringWorkstationBlockEntity extends BlockEntity implement
             dna.setGenes(genes);
             tag.remove(DNA_DATA);
             tag.put(DNA_DATA, dna.serializeNBT());
+            tag.putBoolean(REVEAL_SOURCE, false);
+            tag.putBoolean(REVEAL_STABILITY, false);
+            tag.putBoolean(REVEAL_TRAITS, false);
         }
 
         output.setTag(tag);
