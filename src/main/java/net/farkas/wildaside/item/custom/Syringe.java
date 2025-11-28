3833a77deaaa812b5cd0366299dcb207cb2efab5
Gyula <erdeigyula3@gmail.com>
2025-11-29 00:30:19 +0100

    syringe and dna holder progress

--- src/main/java/net/farkas/wildaside/item/custom/Syringe.java
+++ src/main/java/net/farkas/wildaside/item/custom/Syringe.java
@@ -1,6 +1,7 @@
 package net.farkas.wildaside.item.custom;
 
 import net.farkas.wildaside.capability.dna.DnaCapability;
+import net.farkas.wildaside.capability.dna.DnaImplementation;
 import net.farkas.wildaside.dna.DnaConstants;
 import net.farkas.wildaside.network.NetworkHandler;
 import net.farkas.wildaside.network.packets.SyringeDataPacket;
@@ -79,17 +80,33 @@ public class Syringe extends Item {
 
                 if (target.getUUID() != previousTargetUuid) {
                     tag.putBoolean(UNUSABLE, true);
-                } else {
-                    target.getCapability(DnaCapability.INSTANCE).ifPresent(dna -> {
-                        tag.put(DNA_DATA, dna.serializeNBT());
-                    });
                 }
+
+                target.getCapability(DnaCapability.INSTANCE).ifPresent(dna -> {
+                    tag.put(DNA_DATA, dna.serializeNBT());
+                });
 
                 tag.putUUID(PREVIOUS_TARGET, target.getUUID());
                 blood += NEEDLE_DELTA;
             }
         } else {
             blood -= NEEDLE_DELTA;
+            ItemStack offHandStack = player.getItemInHand(InteractionHand.OFF_HAND);
+            if (offHandStack.getItem() instanceof DnaHolder dnaHolder) {
+                if (progress == 3.0f) {
+                    DnaImplementation dnaImplementation = new DnaImplementation();
+                    dnaImplementation.deserializeNBT(tag.getCompound(DNA_DATA));
+                    if (dnaImplementation.getSource() == null) {
+                        return;
+                    }
+
+                    CompoundTag holderTag = offHandStack.getOrCreateTag();
+                    holderTag.put(DNA_DATA, dnaImplementation.serializeNBT());
+                    int newSampleProgress = Mth.clamp(holderTag.getInt(SAMPLE_PROGRESS) + 1, 0, DnaHolder.DEFAULT_MAX_SAMPLES);
+                    holderTag.putInt(SAMPLE_PROGRESS, newSampleProgress);
+                    offHandStack.setTag(holderTag);
+                }
+            }
         }
 
         blood = Mth.clamp(blood, 0, DEFAULT_MAX_LOAD);
