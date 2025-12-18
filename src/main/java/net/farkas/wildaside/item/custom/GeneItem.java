1b0e7f076191aee4f5b45d1dacbf78c533480184
Gyula <erdeigyula3@gmail.com>
2025-12-18 17:03:16 +0100

    Update GeneItem.java

--- src/main/java/net/farkas/wildaside/item/custom/GeneItem.java
+++ src/main/java/net/farkas/wildaside/item/custom/GeneItem.java
@@ -58,11 +58,11 @@ public class GeneItem extends Item {
 
                     tooltip.add(Component.translatable("dna.wildaside.alleleA").withStyle(ChatFormatting.AQUA));
                     tooltip.add(Component.literal("- ").append(alleleA.getDominance().getComponent()));
-                    tooltip.add(Component.literal("- " + alleleA.getValueHolder().format()));
+                    tooltip.add(Component.literal("- " + alleleA.getValueHolder().format().getString()));
 
                     tooltip.add(Component.translatable("dna.wildaside.alleleB").withStyle(ChatFormatting.AQUA));
                     tooltip.add(Component.literal("- ").append(alleleB.getDominance().getComponent()));
-                    tooltip.add(Component.literal("- " + alleleB.getValueHolder().format()));
+                    tooltip.add(Component.literal("- " + alleleB.getValueHolder().format().getString()));
                 }
             });
         }
